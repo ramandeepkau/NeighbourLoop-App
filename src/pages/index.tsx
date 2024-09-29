1dc105da9a41fed70f05751c0c846e8deb7d8ed6
ramandeepkau <64877434+ramandeepkau@users.noreply.github.com>
2024-09-30 11:56:33 +1300

    change service names and colours

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -203,8 +203,8 @@ const CombinedPage: React.FC = () => {
                         setCurrentPage(4);
                       }}
                     >
-                      <h3 className="text-lg font-bold text-blue-700 mb-1">Service {service.code}</h3>
-                      <p className="text-sm text-gray-700">{service.direction}</p>
+                     
+                     <p className="text-1xl font-bold mb-6" style={{ color: 'darkblue' }}>{service.direction}</p>
                     </div>
                   ))
                 ) : (
